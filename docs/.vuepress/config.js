94daea0c6d8ddbd58f689f58ce6f9ba4ef48442a
1595309412@qq.com <1595309412@qq.com>
2020-07-21 00:05:36 +0800

    frist

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base: "/blog-demo/",
+  title: "blog-demo",
+  description: "Vuepress blog demo",
+  themeConfig: {
+    // 你的GitHub仓库，请正确填写
+    repo: "https://github.com/xxxxxxx/blog-demo",
+    // 自定义仓库链接文字。
+    repoLabel: "My GitHub",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "博客", link: "/blog/01-vue.md" },
+    ],
+    sidebar: [
+      ["/", "首页"],
+      ["/blog/01-vue.md", "我的第一篇博客"],
+    ],
+  },
+};
