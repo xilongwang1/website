8b8278bd2e0cb57bca61540db64f05041310cff7
1595309412@qq.com <1595309412@qq.com>
2020-07-21 00:37:02 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   description: "Vuepress blog demo",
   themeConfig: {
     // 你的GitHub仓库，请正确填写
-    repo: "https://github.com/xilongwang1/blog-demo",
+    repo: "https://github.com/xilongwang1/website",
     // 自定义仓库链接文字。
     repoLabel: "My GitHub",
     nav: [
